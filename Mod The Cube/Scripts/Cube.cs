9ab00ceb4a64dc5007e1f0577b85f8faa1a41d38
j <j>
2022-05-09 16:23:34 -0600

    Mod The Cube namespace Added

--- Mod The Cube/Scripts/Cube.cs
+++ Mod The Cube/Scripts/Cube.cs
@@ -1,46 +1,49 @@
 ﻿using UnityEngine;
 
-public class Cube : MonoBehaviour
+namespace ModTheCube
 {
-    private MeshRenderer Renderer;
-
-    private float lowerLimit = 3;
-    private float topLimit = -30;
-    private float rightLimit = 4;
-    private float leftLimit = -10;
-    private float upLimit = 8;
-    private float downLimit = -9;
-
-    private float xRandomRotation;
-
-    private float counter;
-    
-
-    private void Start()
+    public class Cube : MonoBehaviour
     {
-        Renderer = GetComponent<MeshRenderer>();
+        private MeshRenderer Renderer;
 
-        transform.position = new Vector3(Random.Range(lowerLimit, topLimit), Random.Range(rightLimit, leftLimit), Random.Range(upLimit, downLimit));
-        transform.localScale = Vector3.one * Random.Range(2, 7);
+        private float lowerLimit = 3;
+        private float topLimit = -30;
+        private float rightLimit = 4;
+        private float leftLimit = -10;
+        private float upLimit = 8;
+        private float downLimit = -9;
 
-        xRandomRotation = Random.Range(1.0f, 10.0f) * Time.deltaTime;
-    }
+        private float xRandomRotation;
+
+        private float counter;
 
 
-    private void Update()
-    {
-        // makes the cube rotate
-        transform.Rotate(xRandomRotation, 0, xRandomRotation/2);
-
-        // change the color of the cube every so often
-        Material material = Renderer.material;
-        if (counter <= 0)
+        private void Start()
         {
-            material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+            Renderer = GetComponent<MeshRenderer>();
 
-            counter = 1;
+            transform.position = new Vector3(Random.Range(lowerLimit, topLimit), Random.Range(rightLimit, leftLimit), Random.Range(upLimit, downLimit));
+            transform.localScale = Vector3.one * Random.Range(2, 7);
+
+            xRandomRotation = Random.Range(1.0f, 10.0f) * Time.deltaTime;
         }
 
-        counter -= Time.deltaTime;
+
+        private void Update()
+        {
+            // makes the cube rotate
+            transform.Rotate(xRandomRotation, 0, xRandomRotation / 2);
+
+            // change the color of the cube every so often
+            Material material = Renderer.material;
+            if (counter <= 0)
+            {
+                material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+
+                counter = 1;
+            }
+
+            counter -= Time.deltaTime;
+        }
     }
 }
