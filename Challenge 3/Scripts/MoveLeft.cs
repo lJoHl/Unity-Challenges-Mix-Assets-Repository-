58865570b06381522a5efd8ae0159bc4f11c559e
j <j>
2022-05-09 16:11:23 -0600

    Challenge 3 Assets Were Sorted

---
+++ Challenge 3/Scripts/MoveLeft.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Challenge3
+{
+    public class MoveLeft : MonoBehaviour
+    {
+        public float speed;
+
+        private PlayerController playerControllerScript;
+
+        private float leftBound = -10;
+
+
+        private void Start()
+        {
+            playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        }
+
+
+        private void Update()
+        {
+            // if game is not over, move to the left
+            if (!playerControllerScript.gameOver)
+                transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+
+
+            // if object goes off screen that is NOT the background, destroy it
+            if (transform.position.x < leftBound & !gameObject.CompareTag("Background"))
+                Destroy(gameObject);
+        }
+    }
+}
---
+++ Challenge 3/Scripts/RepeatBackground.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Challenge3
+{
+    public class RepeatBackground : MonoBehaviour
+    {
+        private Vector3 startPos;
+        private float repeatWidth;
+
+
+        private void Start()
+        {
+            startPos = transform.position; // establish the default starting position 
+            repeatWidth = GetComponent<BoxCollider>().size.x / 2; // set repeat width to half of the background
+        }
+
+
+        private void Update()
+        {
+            // if background moves left by its repeat width, move it back to start position
+            if (transform.position.x < startPos.x - repeatWidth)
+                transform.position = startPos;
+        }
+    }
+}
