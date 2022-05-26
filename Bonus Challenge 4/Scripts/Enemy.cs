58bad5154f5270f034a22e56e6619e743697d894
j <j>
2022-05-26 02:04:00 -0600

    Bonus Challenge 4 Expert Difficult Features Added

--- Bonus Challenge 4/Scripts/Enemy.cs
+++ Bonus Challenge 4/Scripts/Enemy.cs
@@ -12,12 +12,22 @@ namespace bonusChallenge4
 
         [SerializeField] private float speed = 3.0f;
 
+        public bool isBoss = false;
+        public float spawnInterval;
+        private float nextSpawn;
+        public int miniEnemySpawnCount;
+        private SpawnManager spawnManager;
+
+
 
         private void Start()
         {
             enemyRb = GetComponent<Rigidbody>();
 
             player = GameObject.Find("Player");
+
+            if (isBoss)
+                spawnManager = FindObjectOfType<SpawnManager>();
         }
 
 
@@ -26,6 +36,15 @@ namespace bonusChallenge4
             Vector3 lookDirection = (player.transform.position - transform.position).normalized;
 
             enemyRb.AddForce(lookDirection * speed);
+
+            if (isBoss)
+            {
+                if (Time.time > nextSpawn)
+                {
+                    nextSpawn = Time.time + spawnInterval;
+                    spawnManager.SpawnMiniEnemy(miniEnemySpawnCount);
+                }
+            }
 
             if (transform.position.y < -10)
                 Destroy(gameObject);
--- Bonus Challenge 4/Scripts/SpawnManager.cs
+++ Bonus Challenge 4/Scripts/SpawnManager.cs
@@ -14,6 +14,10 @@ namespace bonusChallenge4
         public int enemyCount;
         public int waveNumber = 1;
 
+        public GameObject bossPrefab;
+        public GameObject[] miniEnemyPrefabs;
+        public int bossRound;
+
 
         private void Start()
         {
@@ -31,7 +35,10 @@ namespace bonusChallenge4
             if (enemyCount == 0)
             {
                 waveNumber++;
-                SpawnEnemyWave(waveNumber);
+                if (waveNumber % bossRound == 0)
+                    SpawnBossWave(waveNumber);
+                else
+                    SpawnEnemyWave(waveNumber);
 
                 int randomPowerup = Random.Range(0, powerupPrefabs.Length);
                 Instantiate(powerupPrefabs[randomPowerup], GenerateSpawnPosition(), powerupPrefabs[randomPowerup].transform.rotation);
@@ -58,6 +65,35 @@ namespace bonusChallenge4
             Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
 
             return randomPos;
+        }
+
+
+        private void SpawnBossWave(int currentRound)
+        {
+            int miniEnemysToSpawn;
+            //We dont want to divide by 0!
+            if (bossRound != 0)
+            {
+                miniEnemysToSpawn = currentRound / bossRound;
+            }
+            else
+            {
+                miniEnemysToSpawn = 1;
+            }
+            var boss = Instantiate(bossPrefab, GenerateSpawnPosition(),
+            bossPrefab.transform.rotation);
+            boss.GetComponent<Enemy>().miniEnemySpawnCount = miniEnemysToSpawn;
+        }
+
+
+        public void SpawnMiniEnemy(int amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                int randomMini = Random.Range(0, miniEnemyPrefabs.Length);
+                Instantiate(miniEnemyPrefabs[randomMini], GenerateSpawnPosition(),
+                miniEnemyPrefabs[randomMini].transform.rotation);
+            }
         }
     }
 }
