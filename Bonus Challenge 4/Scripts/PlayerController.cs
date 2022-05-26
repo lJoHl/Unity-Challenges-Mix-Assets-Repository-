067969818e950499e59f08b54c8b0af597328191
j <j>
2022-05-26 01:20:51 -0600

    Bonus Challenge 4 Hard Difficult Features Added

--- Bonus Challenge 4/Scripts/PlayerController.cs
+++ Bonus Challenge 4/Scripts/PlayerController.cs
@@ -15,13 +15,18 @@ namespace bonusChallenge4
         public bool hasPowerup = false;
 
         private float powerupStrength = 15.0f;
-
         public GameObject powerupIndicator;
-
         public PowerUpType currentPowerUp = PowerUpType.None;
         public GameObject rocketPrefab;
         private GameObject tmpRocket;
         private Coroutine powerupCountdown;
+
+        public float hangTime;
+        public float smashSpeed;
+        public float explosionForce;
+        public float explosionRadius;
+        private bool smashing = false;
+        private float floorY;
 
 
         private void Start()
@@ -42,6 +47,13 @@ namespace bonusChallenge4
 
             if (currentPowerUp == PowerUpType.Rockets && Input.GetKeyDown(KeyCode.F))
                 LaunchRockets();
+
+            if (currentPowerUp == PowerUpType.Smash & Input.GetKeyDown(KeyCode.Space) & !smashing)
+            {
+                smashing = true;
+                StartCoroutine(Smash());
+            }
+
         }
 
 
@@ -98,5 +110,36 @@ namespace bonusChallenge4
             }
         }
 
+
+        private IEnumerator Smash()
+        {
+            var enemies = FindObjectsOfType<Enemy>();
+            //Store the y position before taking off
+            floorY = transform.position.y;
+            //Calculate the amount of time we will go up
+            float jumpTime = Time.time + hangTime;
+            while (Time.time < jumpTime)
+            {
+                //move the player up while still keeping their x velocity.
+                playerRb.velocity = new Vector2(playerRb.velocity.x, smashSpeed);
+                yield return null;
+            }
+            //Now move the player down
+            while (transform.position.y > floorY)
+            {
+                playerRb.velocity = new Vector2(playerRb.velocity.x, -smashSpeed * 2);
+                yield return null;
+            }
+            //Cycle through all enemies.
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                //Apply an explosion force that originates from our position.
+                if (enemies[i] != null)
+                    enemies[i].GetComponent<Rigidbody>().AddExplosionForce(explosionForce,
+                    transform.position, explosionRadius, 0.0f, ForceMode.Impulse);
+            }
+            //We are no longer smashing, so set the boolean to false
+            smashing = false;
+        }
     }
 }
--- Bonus Challenge 4/Scripts/PowerUp.cs
+++ Bonus Challenge 4/Scripts/PowerUp.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-public enum PowerUpType { None, Pushback, Rockets }
+public enum PowerUpType { None, Pushback, Rockets, Smash }
 
 namespace bonusChallenge4
 {
