feda64c5404ac2883884e0fd87cde450f46f6574
j <j>
2022-05-28 03:19:24 -0600

    Challenge 5 Assets Were Sorted

---
+++ Challenge 5/Scripts/DestroyParticle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace challenge5
+{
+    public class DestroyParticle : MonoBehaviour
+    {
+        // Destroy particle after 2 seconds
+        private void Start()
+        {
+            Destroy(gameObject, 2);
+        }
+    }
+}
---
+++ Challenge 5/Scripts/Target.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace challenge5
+{
+    public class Target : MonoBehaviour
+    {
+        private Rigidbody rb;
+
+        private GameManager gameManager;
+
+        public GameObject explosionFx;
+
+        [SerializeField] private int pointValue;
+
+        private float timeOnScreen = 1.0f;
+
+        public float timeWhenSpawned;
+
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+            timeWhenSpawned = Time.time; 
+
+            StartCoroutine(RemoveObjectRoutine());
+        }
+
+
+        // Sets game behavior, when target is clicked
+        private void OnMouseDown()
+        {
+            if (gameManager.isGameActive)
+            {
+                gameManager.UpdateScore(pointValue);
+
+                Explode();
+                Destroy(gameObject);
+            }
+        }
+
+
+        // Display explosion particle at object's position
+        private void Explode()
+        {
+            Instantiate(explosionFx, transform.position, explosionFx.transform.rotation);
+        }
+
+
+        // After a delay, destroys the target
+        private IEnumerator RemoveObjectRoutine()
+        {
+            yield return new WaitForSeconds(timeOnScreen);
+
+            if (gameManager.isGameActive)
+            {
+                if (!gameObject.CompareTag("Bad"))
+                    gameManager.GameOver();
+                
+                Destroy(gameObject);
+            }
+        }
+
+
+        // Prevents two targets from being within the same space
+        private void OnTriggerEnter(Collider other)
+        {
+            float timeDifference = timeWhenSpawned - other.GetComponent<Target>().timeWhenSpawned;
+
+            if (timeDifference < 0)
+                Destroy(gameObject);
+        }
+    }
+}
