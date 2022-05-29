b117c027c13f6388cafd4f72c78789a6b6963c72
j <j>
2022-05-29 16:51:44 -0600

    Counting Prototype Standard Version

---
+++ Counting Prototype/Counter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    public Text CounterText;
+
+    private int count = 0;
+    private int cubeCount = 0;
+    private int sphereCount = 0;
+    private int cylinderCount = 0;
+
+    private void Start()
+    {
+        count = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Cube":
+                cubeCount++;
+                break;
+
+            case "Sphere":
+                sphereCount++;
+                break;
+
+            case "Cylinder":
+                cylinderCount++;
+                break;
+        }
+
+        count += 1;
+        CounterText.text = $"Count: {count}, Cubes: {cubeCount}, Spheres: {sphereCount}, Cylinders: {cylinderCount}";
+    }
+}
